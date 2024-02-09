735da81c9b7d0705999389ec78da1ec512a02ebb
Jystine <jystine123@gmail.com>
2024-02-08 22:15:17 -0800

    cleaned data

--- todomvc/src/components/App.svelte
+++ todomvc/src/components/App.svelte
@@ -2,5 +2,11 @@
     import { onMount } from "svelte";
     import * as d3 from "d3";
 
-    let data = [];
+    let data;
+    onMount(
+        async() => {
+            data = await d3.csv('https://raw.githubusercontent.com/Jystine/Internet-Usage/main/data/internet_usage.csv')
+        }
+    )
+    $: console.log(data)
 </script>
