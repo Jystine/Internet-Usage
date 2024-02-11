ec24dfff77d9607da94e6c414938f571711aa1e9
Jystine <jystine123@gmail.com>
2024-02-11 00:07:30 -0800

    attempted to map data

--- todomvc/src/components/internet.svelte
+++ todomvc/src/components/internet.svelte
@@ -1,6 +1,8 @@
 <script>
     import * as d3 from "d3";
     import * as topojson from "topojson-client";
+    import { scaleQuantize } from "d3-scale";
+    import { schemeBlues } from "d3-scale-chromatic";
     export let data;
 
     const width = 928;
@@ -16,14 +18,20 @@
     let outline = ({type: "Sphere"});
     let land;
     let border;
+    let countries = [];
+    const colorScale = scaleQuantize([1, 7], schemeBlues[6]);
+    const valuemap = new Map(data.map(d => [d.Region, d.Percentage]));
+    //const valuemap = d3.map(data, d => d.Region);
 
     d3.json(
         "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"
     ).then((world) => {
         land = topojson.feature(world, world.objects.land);
         border = topojson.mesh(world, world.objects.countries, (a, b) => a !== b)
+        countries = topojson.feature(world, world.objects.countries).features;
     });
 
+    $: console.log(countries);
     $: console.log(data);
 </script>
 
@@ -37,7 +45,12 @@
     >
 
     <path d = {path(outline)} fill = "#fff" />
-    <path d = {path(land)} fill = "#000" /> <!-- Map this to the percentage values-->
+    <!-- <path d = {path(land)} fill = "#000" /> --> <!-- Map this to the percentage values-->
+    <g class = "data">
+        {#each countries as country}
+​           <path d = {path(land)} fill={colorScale(valuemap.get(country.properties.name))} />
+​        {/each}
+    </g>
     <path d = {path(border)} fill = "none" stroke = "#fff" />
     <path d = {path(outline)} fill = "none" stroke = "#000" />
 
